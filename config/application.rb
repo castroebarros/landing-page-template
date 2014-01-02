d1e86326badaa17861161017da7fc9b988c829a4
Lucas de Castro <castro.lucas@gmail.com>
2014-01-02 11:08:59 -0300

    Desabilitando precompile assets para rodar no Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module LandingPage
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Desabilitando precompilação dos assets para rodar no Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
